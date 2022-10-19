3e54aa1e43de78f3f4bea58ab122165500b3b3c8
Viktor Larin <Larinv1983@mail.ru>
2022-10-19 12:45:42 +0300

    панель навигации

--- src/componets/header/index.jsx
+++ src/componets/header/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { ReactComponent as LogoIcon } from '../../images/logo.svg';
+import Navigation from '../navigation';
 
 export default function Header() {
 	return (
-		<header>
+		<header className = 'flex'>
 			<LogoIcon />
+			<nav>
+				<Navigation />
+			</nav>
 		</header>
 	);
 }
