5ca45ef658447f13c07fca9f5ee7eea8ae7b79d4
Viktor Larin <Larinv1983@mail.ru>
2022-10-19 15:20:39 +0300

    menu navigations

--- src/componets/navMenu/index.jsx
+++ src/componets/navMenu/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import MenuItem from '../menuItem';
 
-export default function NavMenu({items=[]}) {
+export default function NavMenu({items}) {
 	return (
-		<div>
-			{items.map(({text}) => <span>{text}</span>)}
+		<div className='flex flex-col px-4 py-2 bg-almost-white
+		 drop-shadow-lg rounded-lg absolute top-10 right-0 w-36 space-y-2 z-30'>
+			{items.map(({text, icon}) => 
+				<MenuItem text={text} icon={icon} key={text}/>)}
 		</div>
 	);
 }
