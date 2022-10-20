84f0581747ae1e43dc0149b2c7f14882fc7ab737
Viktor Larin <Larinv1983@mail.ru>
2022-10-20 10:22:08 +0300

    section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
  import Wrapper from './componets/wrapper';
  import Header from './componets/header';
- 
+ import Section from './componets/section/section';
+
 function App() {
   return (
    <Wrapper>
    <Header/>
+   <Section/>
    </Wrapper>
   );  
 }
---
+++ src/componets/section/section.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import MenImage from '../../images/image-hero-desktop.png';
+import MenImageMobile from '../../images/image-hero-mobile.png'
+import { ReactComponent as Databiz } from '../../images/client-databiz.svg'
+import { ReactComponent as Audiophile } from '../../images/client-audiophile.svg'
+import { ReactComponent as Meet } from '../../images/client-meet.svg'
+import { ReactComponent as Maker } from '../../images/client-maker.svg'
+
+export default function Section() {
+	return (
+		<div>
+			Section
+		</div>
+	);
+}
